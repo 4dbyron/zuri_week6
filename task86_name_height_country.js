a06fae2157141b421160e8912976dee8f0f27d0a
Byron Taaka <4dbyron@gmail.com>
2022-06-24 12:35:34 +0300

    Ready For Grading

---
+++ task86_name_height_country.js
@@ -0,0 +1,5 @@
+// JavaScript code that prints out your name, height, and country on the screen
+
+console.log("They Call Me Byron Taaka Oloo.")
+console.log("Height:  158.4 cm")
+console.log("Country: Kenya")
